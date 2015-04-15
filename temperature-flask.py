069791ac1126f585dff5d06fbfdd42f95fcfe90f
josh <jabelch@gmail.com>
2015-04-15 00:36:29 -0600

    Merged temperature and gpio code

--- temperature-flask.py
+++ temperature-flask.py
@@ -1,5 +1,7 @@
 #RESTful Server using Flask
 from flask import Flask, render_template, request, json
+
+import RPi.GPIO as GPIO
 import Adafruit_DHT
 import MySQLdb
 import sys
@@ -7,6 +9,22 @@ import time
 import datetime
 
 app = Flask(__name__)
+
+GPIO.setmode(GPIO.BCM)
+
+#Create a dictionary of pins
+pins = {
+    23 : {'name' : 'Lamp', 'state' : GPIO.IN},
+    24 : {'name' : 'Fan', 'state' : GPIO.IN}
+}
+
+# Helper function to change pin state
+def setPin(pinNumber, dir):
+    GPIO.setup(pinNumber, dir, pull_up_down=GPIO.PUD_UP)
+
+#Set pins as output and low
+for pin in pins:
+    setPin(pin, GPIO.IN)
 
 #Get the top %s temperatures from tempdat 
 selectQuery = """
@@ -36,22 +54,61 @@ def getReading():
     humidity, temp = Adafruit_DHT.read(Adafruit_DHT.DHT11, pin)
     return (humidity, temp)
 
-@app.route("/", methods = ['GET', 'POST'])
+@app.route("/")
 def main():
-    if request.method == 'GET':
-        reading = getFromDatabase(1)
-        templateData = {
-            'tdate' : reading[0][0],
-            'ttime' : reading[0][1],
-            'zone' : reading[0][2],
-            'temperature' : reading[0][3],
-            'humidity' : reading[0][4]
-        }
-        return render_template('temps.html', **templateData)
-    if request.method == 'POST':
-        pass
-    
+    reading = getFromDatabase(1)
+    # Read pin state
+    for pin in pins:
+        pins[pin]['state'] = not GPIO.input(pin)
+
+    templateData = {
+        'tdate' : reading[0][0],
+        'ttime' : reading[0][1],
+        'zone' : reading[0][2],
+        'temperature' : reading[0][3],
+        'humidity' : reading[0][4],
+        'pins' : pins
+    }
+
+    return render_template('temps.html', **templateData)
+
+@app.route("/<changePin>/<action>")
+def action(changePin, action):
+    reading = getFromDatabase(1)
+
+    # Convert pin from URL to integer
+    changePin = int(changePin)
+    # Get name of device being changed
+    deviceName = pins[changePin]['name']
+    # Set pin mode
+    if action == "on":
+        setPin(changePin, GPIO.OUT)
+        message = deviceName + " turned on."
+    if action == "off":
+        setPin(changePin, GPIO.IN)
+        message = deviceName + " turned off."
+    if action == "toggle":
+        setPin(changePin, not GPIO.input(changePin))
+        message = "Toggled " + deviceName + "."
+
+    # Read pin state
+    for pin in pins:
+        pins[pin]['state'] = not GPIO.input(pin)
+
+    templateData = {
+        'tdate' : reading[0][0],
+        'ttime' : reading[0][1],
+        'zone' : reading[0][2],
+        'temperature' : reading[0][3],
+        'humidity' : reading[0][4],
+        'message' : message,
+        'pins' : pins
+    }
+
+    return render_template('main.html', **templateData)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
-    #Close cursor and database
+    #turn off pins
+    for pin in pins:
+        setPin(pin, GPIO.IN)
