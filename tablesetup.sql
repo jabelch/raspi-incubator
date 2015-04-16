a1b52ac5416636de24980cf7f6311637466be650
Josh <jabelch@gmail.com>
2015-04-15 22:15:00 -0600

    Update tablesetup.sql

--- tablesetup.sql
+++ tablesetup.sql
@@ -2,9 +2,11 @@ CREATE DATABASE temps;
 USE temps
 
 CREATE TABLE IF NOT EXISTS `setpoints` (
-    `id` int(3) NOT NULL AUTO_INCREMENT,
+    `id` int(10) NOT NULL,
     `sp_high` numeric NOT NULL,
     `sp_low` numeric NOT NULL
+    UNIQUE (`id`),
+    PRIMARY KEY (`id`)
 );
   
 CREATE TABLE IF NOT EXISTS `tempdat` (
