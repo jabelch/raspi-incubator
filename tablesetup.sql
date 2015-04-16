5df346beaded27df61b1c4d4681c72a774991193
Josh <jabelch@gmail.com>
2015-04-15 22:08:37 -0600

    Create tablesetup.sql

---
+++ tablesetup.sql
@@ -0,0 +1,18 @@
+CREATE DB temps
+USE temps
+
+CREATE TABLE IF NOT EXISTS `setpoints` (
+    `id` int(3) NOT NULL AUTO_INCREMENT,
+    `sp_high` numeric NOT NULL,
+    `sp_low` numeric NOT NULL
+);
+  
+CREATE TABLE IF NOT EXISTS `tempdat` (
+    `tdatetime` TIMESTAMP NOT NULL,
+    `zone` VARCHAR(64),
+    `temperature` NUMERIC NOT NULL,
+    `humidity` NUMERIC
+);
+
+INSERT INTO `setpoints` (`id`, `sp_high`, `sp_low`)
+VALUES(1, 102, 97);
