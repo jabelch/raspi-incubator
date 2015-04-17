99d67336902dc121ab22ecc762156b16108b7886
Josh <jabelch@gmail.com>
2015-04-17 01:04:02 -0600

    Added additional setpoints for fan controls
    Humidity and emergency temperature use required additional set points.  Modified database.  Also updated the temperature in setpoints to be Celsius to match the readings from the DHT11 Thermometer.

--- tablesetup.sql
+++ tablesetup.sql
@@ -3,8 +3,12 @@ USE temps
 
 CREATE TABLE IF NOT EXISTS `setpoints` (
     `id` int(10) NOT NULL,
-    `sp_high` numeric NOT NULL,
-    `sp_low` numeric NOT NULL
+    `sp_high` NUMERIC NOT NULL,
+    `sp_low` NUMERIC NOT NULL,
+    `sp_too_hot` NUMERIC NOT NULL,
+    `sp_high_h, NUMERIC NOT NULL,
+    `sp_low_h, NUMERIC NOT NULL,
+    `sp_too_humid`, NUMERIC NOT NULL,
     UNIQUE (`id`),
     PRIMARY KEY (`id`)
 );
@@ -17,18 +21,18 @@ CREATE TABLE IF NOT EXISTS `tempdat` (
 );
 
 CREATE TABLE IF NOT EXISTS `pins` (
-    `id` int(3) NOT NULL,
-    `pin` int(2) NOT NULL,
-    `name` varchar(64),
+    `id` INT(3) NOT NULL,
+    `pin` INT(2) NOT NULL,
+    `name` VARCHAR(64),
     UNIQUE(`pin`),
     PRIMARY KEY(`id`)
 );
 
-INSERT INTO `setpoints` (`id`, `sp_high`, `sp_low`)
-VALUES(1, 102, 97);
+INSERT INTO `setpoints` (`id`, `sp_high`, `sp_low`, `sp_too_hot`, `sp_high_h`, `sp_low_h`, `sp_too_humid`)
+VALUES(1, 38, 38, 40, 52, 42, 62);
 
 INSERT INTO `pins` (`id`, `pin`, `name`)
 VALUES(1, 23, "Lamp"), 
-(2, 24, "Fan"), 
-(3, 25, "IN3"), 
-(4, 8, "IN4");
+(2, 24, "Circulation Fan"), 
+(3, 25, "Emergency Fan"), 
+(4, 8, "Humidity Fan");
